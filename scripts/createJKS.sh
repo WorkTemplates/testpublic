2869cc747d07ab80d94fbc3bcda0eb8ec524bd1b
Evgeniy Petrov <geka000@gmail.com>
2021-06-04 18:23:12 +0300

    add python script to workflow

--- scripts/createJKS.sh
+++ scripts/createJKS.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-#echo y | keytool -genkeypair -dname "cn=$1 $2" -alias release -keypass o3s3cfJF8RJd -keystore app/$1$2.jks -storepass 7iCR10OgB5ma -validity 20000 -keyalg RSA -keysize 2048
+keytool -genkeypair -dname "cn=$1 $2" -alias release -keypass AWzLF2GuP9khYVLq -keystore app/keystore.jks -storepass AWzLF2GuP9khYVLq -validity 20000 -keyalg RSA -keysize 2048 -storetype JKS
